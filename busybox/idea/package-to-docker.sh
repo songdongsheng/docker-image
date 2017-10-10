6983ceb78912b8518133584a5798e57a4b16722c
宋冬生 <songdongsheng@live.cn>
2017-10-10 04:37:34 +0000

    Add tag 1.5 on busybox/idea

--- busybox/idea/package-to-docker.sh
+++ busybox/idea/package-to-docker.sh
@@ -2,7 +2,9 @@
 
 docker build -t songdongsheng/idea:busybox_1.27.2-idea_1.5 -f Dockerfile .
 docker tag      songdongsheng/idea:busybox_1.27.2-idea_1.5 songdongsheng/idea:busybox
+docker tag      songdongsheng/idea:busybox_1.27.2-idea_1.5 songdongsheng/idea:1.5
 docker tag      songdongsheng/idea:busybox_1.27.2-idea_1.5 songdongsheng/idea:latest
 docker push     songdongsheng/idea:busybox_1.27.2-idea_1.5
 docker push     songdongsheng/idea:busybox
+docker push     songdongsheng/idea:1.5
 docker push     songdongsheng/idea:latest
