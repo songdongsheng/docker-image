d782d6290ee4e819a4ad3b15ab196bddb24ccbf4
宋冬生 <songdongsheng@live.cn>
2019-01-16 14:07:07 +0800

    Update to BusyBox 1.30.0

--- busybox/idea/package-to-docker.sh
+++ busybox/idea/package-to-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t songdongsheng/idea:busybox_1.29.3-idea_1.6 -f Dockerfile .
-docker tag      songdongsheng/idea:busybox_1.29.3-idea_1.6 songdongsheng/idea:busybox
-docker tag      songdongsheng/idea:busybox_1.29.3-idea_1.6 songdongsheng/idea:1.6
-docker tag      songdongsheng/idea:busybox_1.29.3-idea_1.6 songdongsheng/idea:latest
-docker push     songdongsheng/idea:busybox_1.29.3-idea_1.6
+docker build -t songdongsheng/idea:busybox_1.30.0-idea_1.6 -f Dockerfile .
+docker tag      songdongsheng/idea:busybox_1.30.0-idea_1.6 songdongsheng/idea:busybox
+docker tag      songdongsheng/idea:busybox_1.30.0-idea_1.6 songdongsheng/idea:1.6
+docker tag      songdongsheng/idea:busybox_1.30.0-idea_1.6 songdongsheng/idea:latest
+docker push     songdongsheng/idea:busybox_1.30.0-idea_1.6
 docker push     songdongsheng/idea:busybox
 docker push     songdongsheng/idea:1.6
 docker push     songdongsheng/idea:latest
