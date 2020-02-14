eab07fbb02a9dd4346fd51a3b98f475ff829e565
Dongsheng Song <dongsheng.song@gmail.com>
2020-02-14 10:59:22 +0800

    Remove old stuff

--- alpine/squid/package-to-docker.sh
+++ alpine/squid/package-to-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+set -Eeu -o pipefail
+
 INSTALL_ROOT=$(mktemp -p . -d)
 cd $INSTALL_ROOT
 echo $INSTALL_ROOT
 
 # use minirootfs
-curl -sSO http://dl-cdn.alpinelinux.org/alpine/v3.8/releases/x86_64/alpine-minirootfs-3.8.2-x86_64.tar.gz
-tar -xzf alpine-minirootfs-3.8.2-x86_64.tar.gz
-rm -f alpine-minirootfs-3.8.2-x86_64.tar.gz
+curl -sS http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.4-x86_64.tar.gz | tar -xvzf -
 
 # apk add squid
 cat << EOF | tee add_squid.sh
@@ -21,15 +21,15 @@ chroot . /bin/sh /add_squid.sh
 cp ../squid.conf etc/squid/
 mv manifest ../ && rm -f add_squid.sh
 
-echo SQUID_VERSION:${SQUID_VERSION}
-
 # rootfs.tar.xz
 tar -cvJf ../rootfs.tar.xz *
 cd ..
 rm -fr $INSTALL_ROOT
 
 SQUID_VERSION=`cat manifest  | grep squid`
+echo SQUID_VERSION:${SQUID_VERSION}
 SQUID_VERSION=${SQUID_VERSION:6}
+echo SQUID_VERSION:${SQUID_VERSION}
 
 # docker images
 TS=`/bin/date +"%Y%m%d_%H%M%S"`
@@ -48,6 +48,9 @@ EOF
 docker build -t songdongsheng/squid:${TS} -f Dockerfile-${TS} .
 docker tag      songdongsheng/squid:${TS} songdongsheng/squid:${SQUID_VERSION}
 docker tag      songdongsheng/squid:${TS} songdongsheng/squid:latest
+
+exit 0
+
 docker push     songdongsheng/squid:${TS}
 docker push     songdongsheng/squid:${SQUID_VERSION}
 docker push     songdongsheng/squid:latest
