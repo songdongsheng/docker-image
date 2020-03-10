89175d2928efde708964cca7a265733b9f5a36cb
宋冬生 <songdongsheng@live.cn>
2020-03-10 22:33:23 +0800

    Add PowerShell containers

--- nanoserver/build.cmd
+++ nanoserver/build.cmd
@@ -2,40 +2,29 @@
 @SET CD_HOME=%CD%
 
 @rem echo %JDN_HOME%
+@rem https://hub.docker.com/_/microsoft-powershell
+@rem docker pull mcr.microsoft.com/powershell:lts-nanoserver-1809
+@rem docker pull mcr.microsoft.com/powershell:nanoserver-1809
+@rem docker pull mcr.microsoft.com/powershell:lts-debian-10
+@rem docker pull mcr.microsoft.com/powershell:debian-10
+@rem docker pull mcr.microsoft.com/powershell:lts-ubuntu-18.04
+@rem docker pull mcr.microsoft.com/powershell:ubuntu-18.04
 
-@cd %JDN_HOME%jdk-6-latest
-docker build --force-rm -t songdongsheng/openjdk:6 -f Dockerfile .
-docker tag songdongsheng/openjdk:6 songdongsheng/openjdk:6-1.6.0_97
-docker tag songdongsheng/openjdk:6 songdongsheng/openjdk:6-1.6.0_97-zulu_6.17.0.1
-
-@cd %JDN_HOME%jdk-7-latest
-docker build --force-rm -t songdongsheng/openjdk:7 -f Dockerfile .
-docker tag songdongsheng/openjdk:7 songdongsheng/openjdk:7-1.7.0_154
-docker tag songdongsheng/openjdk:7 songdongsheng/openjdk:7-1.7.0_154-zulu_7.20.0.3
-
-@cd %JDN_HOME%jdk-8-latest
+@cd %JDN_HOME%Java-8
 docker build --force-rm -t songdongsheng/openjdk:8 -f Dockerfile .
 docker tag songdongsheng/openjdk:8 songdongsheng/openjdk:8-1.8.0_144
 docker tag songdongsheng/openjdk:8 songdongsheng/openjdk:8-1.8.0_144-zulu_8.23.0.3
 docker tag songdongsheng/openjdk:8 songdongsheng/openjdk:latest
 
-@cd %JDN_HOME%jdk-9-latest
+@cd %JDN_HOME%Java-11
 docker build --force-rm -t songdongsheng/openjdk:9 -f Dockerfile .
 docker tag songdongsheng/openjdk:9 songdongsheng/openjdk:9
 docker tag songdongsheng/openjdk:9 songdongsheng/openjdk:9-zulu_9.0.0.15
 
-docker push songdongsheng/openjdk:6
-docker push songdongsheng/openjdk:6-1.6.0_97
-docker push songdongsheng/openjdk:6-1.6.0_97-zulu_6.17.0.1
-docker push songdongsheng/openjdk:7
-docker push songdongsheng/openjdk:7-1.7.0_154
-docker push songdongsheng/openjdk:7-1.7.0_154-zulu_7.20.0.3
-docker push songdongsheng/openjdk:latest
+@cd %CD_HOME%
+
 docker push songdongsheng/openjdk:8
 docker push songdongsheng/openjdk:8-1.8.0_144
 docker push songdongsheng/openjdk:8-1.8.0_144-zulu_8.23.0.3
-docker push songdongsheng/openjdk:9
-docker push songdongsheng/openjdk:9-zulu_9.0.0.15
-@cd %CD_HOME%
 
 pause
